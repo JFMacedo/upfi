8d65290e40cb9d4d34ef64a1c04730fb5ae99118
Jean Fernandes de Macedo <jfmacedo91@gmail.com>
2021-09-18 09:12:59 -0300

    :package: added loader and error components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,8 +15,7 @@ export default function Home(): JSX.Element {
         after: pageParam
       }
     })
-
-    console.log(data)
+    
     return data
   }
 
@@ -34,12 +33,18 @@ export default function Home(): JSX.Element {
   );
 
   const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
+    const formatted = data?.pages.flatMap(imagesData => {
+      return imagesData.data.flat()
+    })
   }, [data]);
 
-  // TODO RENDER LOADING SCREEN
+  if(isLoading && !isError) {
+    return <Loading />
+  }
 
-  // TODO RENDER ERROR SCREEN
+  if(isLoading && isError) {
+    return <Error />
+  }
 
   return (
     <>
