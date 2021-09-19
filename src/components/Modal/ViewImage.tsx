80f3db9b4e76b987d2694961b2323b3e5f7c7266
Jean Fernandes de Macedo <jfmacedo91@gmail.com>
2021-09-19 07:39:19 -0300

    :package: added modal view image

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -19,5 +19,17 @@ export function ModalViewImage({
   onClose,
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
-  return <></>
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} isCentered size="4xl">
+      <ModalOverlay />
+      <ModalContent mx="auto" w="auto" h="auto" maxW="900px" maxH="600px">
+        <ModalBody p="0">
+          <Image src={imgUrl} maxW="900px" maxH="600px" />
+        </ModalBody>
+        <ModalFooter h="8" py="5" borderBottomRadius="5px" bgColor="pGray.800">
+          <Link href={imgUrl} fontSize="md" mr="auto" isExternal>Abrir original</Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  )
 }
